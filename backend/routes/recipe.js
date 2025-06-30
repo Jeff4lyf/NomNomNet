a0ac94c458ddbe68ae48b9c4eb75299ff5ca8e43
Jeff4lyf <jeffjpraj@gmail.com>
2025-06-30 13:49:02 +0530

    Removed useless comments

--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -10,7 +10,7 @@ const {
 const requireAuth = require('../middleware/requireAuth');
 const router = express.Router();
 
-//router.use(requireAuth);
+router.use(requireAuth);
 router.get("/recipes", getAll);
 router.get("/recipe/:id",getrecipe);
 router.get("/recipe/user", getUserRecipes);
